f36b6a82106d16fdee0ef19749e2e9f95f482db9
Fabian-Krupinski-Projektaccount <github.hdfavo@gmail.com>
2020-11-10 14:50:27 +0100

    .

--- bot.js
+++ bot.js
@@ -16,27 +16,43 @@ var conf = {
 
 
 
-const client = new Discord.Client();
-client.login(process.env.DISCORD_BOT_TOKEN);
-client.commands = new Discord.Collection();
-client.prefix = conf.prefix;
-client.name = conf.name;
+const bot_token_list = [
+    process.env.DISCORD_BOT_TOKEN0,
+    process.env.DISCORD_BOT_TOKEN1,
+    process.env.DISCORD_BOT_TOKEN2,
+    process.env.DISCORD_BOT_TOKEN3
+];
+var client_list = [];
 
-client.guild_list = {};
+bot_token_list.forEach(token => {
+    let client_id = client_list.length;
+
+    client_list[client_id] = new Discord.Client()
+
+    client_list[client_id].login(token);
+    client_list[client_id].commands = new Discord.Collection();
+    client_list[client_id].prefix = conf.prefix;
+    client_list[client_id].name = conf.name;
+
+    client_list[client_id].guild_list = {};
+});
+
 
 
 /**
  * Client Events
  */
-client.on('ready', () => {
-    console.log(`Logged in as >>${client.user.tag}<<`);
+client_list.forEach(client => {
+    client.on('ready', () => {
+        console.log(`Logged in as >>${client.user.tag}<<`);
 
-    client.user.setPresence({
-        activity: {
-            name: 'Music', type: 'PLAYING'
-        },
-        status: 'online'
-    })
+        client.user.setPresence({
+            activity: {
+                name: 'Music', type: 'PLAYING'
+            },
+            status: 'online'
+        })
+    });
 });
 
 /**
@@ -47,7 +63,9 @@ const commandFiles = fs.readdirSync(path.join(__dirname, 'commands'))
 
 for (const file of commandFiles) {
 	const command = require(path.join(__dirname, 'commands', `${file}`));
-	client.commands.set(command.name, command);
+    client_list.forEach(client => {
+	       client.commands.set(command.name, command);
+    });
 	console.log(`Command >>\x1b[4m${command.name}\x1b[0m<< \x1b[32mloaded!\x1b[0m`);
 }
 
@@ -56,43 +74,44 @@ for (const file of commandFiles) {
 
 
 
+client_list.forEach(client => {
+    client.on('message', async msg => {
+    	if (msg.author.bot) return;
+    	if (!msg.content.startsWith(client.prefix)) return;
 
-client.on('message', async msg => {
-	if (msg.author.bot) return;
-	if (!msg.content.startsWith(client.prefix)) return;
+    	const args = msg.content.slice(client.prefix.length).trim().split(/ +/g);
+    	const commandName = args.shift().toLowerCase();
 
-	const args = msg.content.slice(client.prefix.length).trim().split(/ +/g);
-	const commandName = args.shift().toLowerCase();
+    	const command =
+    		client.commands.get(commandName) ||
+    		client.commands.find((cmd) => cmd.aliases && cmd.aliases.includes(commandName));
 
-	const command =
-		client.commands.get(commandName) ||
-		client.commands.find((cmd) => cmd.aliases && cmd.aliases.includes(commandName));
+    	if (!command) return;
 
-	if (!command) return;
-
-    if (!client.guild_list[msg.guild.id]) {
-        client.guild_list[msg.guild.id] = {
-            music: {
-                queue: new Map(),
-                isPlaying: false,
-                dispatcher: null,
-                voiceChannel: null,
-                volume: 5
-            }
-        };
-    }
-
-
+        if (!client.guild_list[msg.guild.id]) {
+            client.guild_list[msg.guild.id] = {
+                music: {
+                    queue: new Map(),
+                    isPlaying: false,
+                    dispatcher: null,
+                    voiceChannel: null,
+                    volume: 5
+                }
+            };
+        }
 
 
 
 
-	try {
-		command.execute(msg, args, client);
-	} catch (error) {
-		console.error(error);
-		msg.reply("There was an error executing that command.").catch(console.error);
-	}
+
+
+    	try {
+    		command.execute(msg, args, client);
+    	} catch (error) {
+    		console.error(error);
+    		msg.reply("There was an error executing that command.").catch(console.error);
+    	}
+    });
 });
 
 //https://www.voidcanvas.com/make-console-log-output-colorful-and-stylish-in-browser-node/
