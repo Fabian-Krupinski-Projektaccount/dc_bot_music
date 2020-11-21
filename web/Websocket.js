4bc195c39c7343b720561c27c4e6d40b581ce355
Fabian-Krupinski-Projektaccount <github.hdfavo@gmail.com>
2020-11-21 19:53:55 +0100

    .

--- web/Websocket.js
+++ web/Websocket.js
@@ -29,68 +29,11 @@ class WebSocket {
         this.app.use(bodyParser.urlencoded({ extended: false }));
         this.app.use(bodyParser.json());
 
-        //this.registerRoots();
         this.routes = new Routes(this.app, this.token, this.bot_list)
         this.routes.register();
 
         this.server = this.app.listen(this.port, () => {
             console.log("Websocket API set up at port " + this.server.address().port)
-        });
-    }
-
-    checkToken(_token) {
-        return (_token == this.token);
-    }
-
-    registerRoots() {
-        this.app.get('/', (req, res) => {
-            var _token = req.query.token;
-
-            if (!this.checkToken(_token)) {
-                res.render('error', { title: "ERROR" });
-                return;
-            }
-
-
-            var text_channels = [];
-            var voice_channels = [];
-            this.client.guilds.cache.first().channels.cache
-                .forEach(c => {
-                    if(c.type == 'text') {
-                        text_channels.push({id: c.id, name: c.name});
-                    } else if(c.type == 'voice') {
-                        voice_channels.push({id: c.id, name: c.name});
-                    }
-                });
-
-            res.render('index', {
-                title: "Web Interface",
-                token: _token,
-                text_channels,
-                voice_channels
-            });
-        });
-
-        this.app.post('/sendMessage', (req, res) => {
-            var _token = req.body.token;
-            var channelid = req.body.channelid;
-            var text = req.body.text;
-
-            if(!_token || !channelid || !text) {
-                return res.sendStatus(400);
-            }
-            if (!this.checkToken(_token)) {
-                return res.sendStatus(401);
-            }
-
-
-            var channel = this.client.guilds.cache.first().channels.cache.get(channelid);
-            if(channel) {
-                channel.send(text);
-                res.sendStatus(200);
-            } else {
-                res.sendStatus(406);
-            }
         });
     }
 }
